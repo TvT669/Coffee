b381cf4429cb2b46b9213e14414cb90b17214192
bee <3172736837@qq.com>
2025-03-04 15:54:06 +0800

    Initial Commit

---
+++ Coffee/CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp.swift
+//  Coffee
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/3/4.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeTests/CoffeeTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoffeeTests.swift
+//  CoffeeTests
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/3/4.
+//
+
+import Testing
+
+struct CoffeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
